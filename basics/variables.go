6be53e985f02d906179b2d467cfde90ae8d9b24a
flsh <fenglianshuang@jiuan.com>
2018-02-01 16:19:17 +0800

    创建项目

---
+++ basics/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var code, python, java bool
+var i, j = 1, 2 //初始化变量
+
+func main() {
+
+	 a, b, c := true, "java", 123  //短声名变量
+	 fmt.Println(code,python,java)
+	 fmt.Println(i, j)
+	 fmt.Println(a,b,c)
+}
